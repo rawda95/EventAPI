cf8612ac0769482fef7fb07a7ba999622af5277d
rawda <rawda95.r@gmail.com>
2019-12-09 21:36:13 +0200

    change db to remote

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 //local connect 
-mongoose.connect('mongodb://127.0.0.1:27017/Events', { useNewUrlParser: true, useFindAndModify: false });
+//mongoose.connect('mongodb://127.0.0.1:27017/Events', { useNewUrlParser: true, useFindAndModify: false });
 
 
 // remote onnect 
-// mongoose.connect('mongodb+srv://admin:admin@exmainations-scodw.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin:admin@event-zkksp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 mongoose.connection.on('error', error => console.log(error));
 mongoose.connection.on('connected', function() {
     console.log('connectd to db ');
